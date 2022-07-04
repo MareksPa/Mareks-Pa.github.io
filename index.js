c0e597b7f3f6043cb54d421419109f9361358a80
MareksPa <mr.cygopat@gmail.com>
2022-07-04 15:32:10 +0300

    added new function

--- index.js
+++ index.js
@@ -3,5 +3,21 @@ const nuke = document.querySelector('#clickToNuke');
 nuke.addEventListener('click', updateName);
 
 function updateName() {
-  nuke.innerText   = 'This has been nuked';
+
+  nuke.innerText   = 'This has been nuked ' + "times";
+
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+  function createParagraph() {
+    const para = document.createElement('p');
+    para.textContent = 'You clicked the button!';
+    document.body.appendChild(para);
+  }
+
+  const buttons = document.querySelectorAll('button');
+
+  for (const button of buttons) {
+    button.addEventListener('click', createParagraph);
+  }
+});
